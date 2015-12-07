338117c4aa1d5b65ba393779b23d97871634352a
Chet Kener <Cl3Kener@gmail.com>
2015-12-07 13:03:02 -0500

    Bring uber.mk up to speed
    * First off with 2016 right around the corner we need to future proof uber.mk for when the toolchains switch to 2016.
    * We need to add aarch64 custom kernel support or just define ROM toolchain as kernel toolchain if not using a custom toolchain.
    * Filter in cases where date is missing (for example UBERTC-4.8 is a release and releases don't come with dates) so that build.prop entry doesn't have a hyphen hanging out.
    * Reformat, simplify, and clump like with like.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	config/uber.mk
    
    Change-Id: Ic449510472b7784be64e1d9248ecafac35c59743

--- config/uber.mk
+++ config/uber.mk
@@ -1,100 +1,112 @@
-
-# Written for UBER toolchains
-# Find host os
-
-# Set GCC colors
-export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+# Written for UBER toolchains (UBERTC)
+# Requires a Linux Host
 
 UNAME := $(shell uname -s)
-
 ifeq (Linux,$(UNAME))
   HOST_OS := linux
 endif
 
 ifeq (linux,$(HOST_OS))
 ifeq (arm,$(TARGET_ARCH))
-# Path to toolchain
+# ANDROIDEABI TOOLCHAIN INFO
 AND_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
 AND_TC_VERSION := $(shell $(AND_TC_PATH)/bin/arm-linux-androideabi-gcc --version 2>&1)
 AND_TC_VERSION_NUMBER := $(shell $(AND_TC_PATH)/bin/arm-linux-androideabi-gcc -dumpversion 2>&1)
-
-# Find strings in version info
+AND_TC_DATE := $(filter 20150% 20151% 20160% 20161%,$(AND_TC_VERSION))
 ifneq ($(filter (UBERTC%),$(AND_TC_VERSION)),)
-AND_TC_NAME := UBERTC
+  AND_TC_NAME := UBERTC
 else
-AND_TC_NAME := GCC
+  AND_TC_NAME := GCC
 endif
-
-AND_TC_DATE := $(filter 20150% 20151% ,$(AND_TC_VERSION))
-ifeq ($(AND_TC_DATE),)
-	AND_TC_DATE := $(filter 2014% ,$(AND_TC_VERSION))
+ifeq (,$(AND_TC_DATE))
+  ARM_AND_PROP := $(AND_TC_NAME)-$(AND_TC_VERSION_NUMBER)
+else
+  ARM_AND_PROP := ($(AND_TC_NAME)-$(AND_TC_VERSION_NUMBER))-$(AND_TC_DATE)
 endif
-
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.uber.android=($(AND_TC_NAME)-$(AND_TC_VERSION_NUMBER))-$(AND_TC_DATE)
+    ro.uber.android=$(ARM_AND_PROP)
 
+# ARM-EABI TOOLCHAIN INFO
 ifneq ($(TARGET_GCC_VERSION_ARM),)
-KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)
+  KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)
 else
-KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION)
+  KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION)
 endif
-
 KERNEL_TC_VERSION := $(shell $(KERNEL_TC_PATH)/bin/arm-eabi-gcc --version 2>&1)
 KERNEL_TC_VERSION_NUMBER := $(shell $(KERNEL_TC_PATH)/bin/arm-eabi-gcc -dumpversion 2>&1)
-
-# Find strings in version info
+KERNEL_TC_DATE := $(filter 20150% 20151% 20160% 20161%,$(KERNEL_TC_VERSION))
 ifneq ($(filter (UBERTC%),$(KERNEL_TC_VERSION)),)
-KERNEL_TC_NAME := UBERTC
+  KERNEL_TC_NAME := UBERTC
 else
-KERNEL_TC_NAME := GCC
+  KERNEL_TC_NAME := GCC
 endif
-
-KERNEL_TC_DATE := $(filter 20150% 20151% ,$(KERNEL_TC_VERSION))
-
-ifeq ($(KERNEL_TC_DATE),)
-	KERNEL_TC_DATE := $(filter 2014% ,$(KERNEL_TC_VERSION))
+ifeq (,$(KERNEL_TC_DATE))
+  ARM_KERNEL_PROP := $(KERNEL_TC_NAME)-$(KERNEL_TC_VERSION_NUMBER)
+else
+  ARM_KERNEL_PROP := ($(KERNEL_TC_NAME)-$(KERNEL_TC_VERSION_NUMBER))-$(KERNEL_TC_DATE)
 endif
-
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.uber.kernel=($(KERNEL_TC_NAME)-$(KERNEL_TC_VERSION_NUMBER))-$(KERNEL_TC_DATE)
-
+    ro.uber.kernel=$(ARM_KERNEL_PROP)
 endif
 
 ifeq (arm64,$(TARGET_ARCH))
-# Path to toolchain
+# AARCH64 ROM TOOLCHAIN INFO
 UBER_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
 UBER_AND := $(shell $(UBER_AND_PATH)/bin/aarch64-linux-android-gcc --version)
-
-# Find strings in version info
+UBER_AND_VERSION_NUMBER := $(shell $(AND_TC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
+UBER_AND_DATE := $(filter 20150% 20151% 20160% 20161%,$(UBER_AND))
 ifneq ($(filter (UBERTC%),$(UBER_AND)),)
-UBER_AND_NAME := $(filter (UBERTC%),$(UBER_AND))
-UBER_AND_DATE := $(filter 20150% 20151%,$(UBER_AND))
-UBER_AND_VERSION := $(UBER_AND_NAME)-$(UBER_AND_DATE)
+  UBER_AND_NAME := UBERTC
+else
+  UBER_AND_NAME := GCC
+endif
+ifeq (,$(UBER_AND_DATE))
+  AARCH64_AND_PROP := $(UBER_AND_NAME)-$(UBER_AND_VERSION_NUMBER)
+else
+  AARCH64_AND_PROP := ($(UBER_AND_NAME)-$(UBER_AND_VERSION_NUMBER))-$(UBER_AND_DATE)
+endif
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.uber.android=$(UBER_AND_VERSION)
+    ro.uber.android=$(AARCH64_AND_PROP)
+
+# AARCH64 KERNEL TOOLCHAIN INFO
+ifneq ($(TARGET_GCC_VERSION_ARM64),)
+  UBER_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_VERSION_ARM64)
+else
+  UBER_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
 endif
+UBER_TC_VERSION := $(shell $(UBER_TC_PATH)/bin/aarch64-linux-android-gcc --version)
+UBER_TC_VERSION_NUMBER := $(shell $(UBER_TC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
+UBER_TC_DATE := $(filter 20150% 20151% 20160% 20161%,$(UBER_TC_VERSION))
+ifneq ($(filter (UBERTC%),$(UBER_TC_VERSION)),)
+  UBER_TC_NAME := UBERTC
+else
+  UBER_TC_NAME := GCC
+endif
+ifeq (,$(UBER_TC_DATE))
+  AARCH64_KERNEL_PROP := $(UBER_TC_NAME)-$(UBER_TC_VERSION_NUMBER)
+else
+  AARCH64_KERNEL_PROP := ($(UBER_TC_NAME)-$(UBER_TC_VERSION_NUMBER))-$(UBER_TC_DATE)
+endif
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.uber.kernel=$(AARCH64_KERNEL_PROP)
 endif
 
+# UBERTC OPTIMIZATIONS 
 ifeq (true,$(STRICT_ALIASING))
-OPT1 := (strict)
+  OPT1 := (strict)
 endif
-
 ifeq (true,$(GRAPHITE_OPTS))
-OPT2 := (graphite)
+  OPT2 := (graphite)
 endif
-
 ifeq (true,$(KRAIT_TUNINGS))
-OPT3 := ($(TARGET_CPU_VARIANT))
+  OPT3 := ($(TARGET_CPU_VARIANT))
 endif
-
 ifeq (true,$(ENABLE_GCCONLY))
-OPT4 := (gcconly)
+  OPT4 := (gcconly)
 endif
-
 ifeq (true,$(CLANG_O3))
-OPT5 := (clang_O3)
+  OPT5 := (clang_O3)
 endif
-
 GCC_OPTIMIZATION_LEVELS := $(OPT1)$(OPT2)$(OPT3)$(OPT4)$(OPT5)
 ifneq (,$(GCC_OPTIMIZATION_LEVELS))
 ADDITIONAL_BUILD_PROPERTIES += \
