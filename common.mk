5544996739501bb62fc748d166ecac9eecee5963
Adarsh-MR <adarshmr1998@gmail.com>
2016-08-10 10:01:19 +0000

    bump to 1.3
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- common.mk
+++ common.mk
@@ -171,7 +171,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 # Versioning System
 # Citrus-CAF first version.
 PRODUCT_VERSION_MAJOR = MM
-PRODUCT_VERSION_MINOR = 1.1
+PRODUCT_VERSION_MINOR = 1.3
 ifdef CITRUS_BUILD_EXTRA
     CITRUS_POSTFIX := -$(CITRUS_BUILD_EXTRA)
 endif
