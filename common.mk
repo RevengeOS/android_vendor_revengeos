DirtyUnicorns/android_vendor_du

ADB over network [2/3]
Allow SystemServer to set service.adb.tcp.* properties
Required for network adb enable/disable to function

sepolicy: Fix permissions for service.adb.tcp.port
This makes the rule more specific by overriding the upstream sepolicy.
Also adds the adbd context which is necessary for "adb tcpip".

SELinux: Use custom ADB over network property
* Use a custom system property to trigger the real one, so we avoid
  running afoul of any SELinux CTS requirements.

This makes the rule more specific by overriding the upstream sepolicy.
Also adds the adbd context which is necessary for "adb tcpip".

Change-Id: Ie5636390ee34a99090b5fa4ca2123019044c7c84

config/common.mk
@@ -8,6 +8,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/du/prebuilt/etc/init.du.rc:root/init.du.rc
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \

RevengeOS/android_vendor_revengeos

common.mk
@@ -16,6 +16,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
+PRODUCT_COPY_FILES += \
+    vendor/citrus/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/citrus/prebuilt/etc/init.citrus.rc:root/init.citrus.rc
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \