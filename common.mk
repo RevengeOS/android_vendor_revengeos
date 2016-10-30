7a4740c8ae430ed681ae23d71837c317d4204ddb
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2016-10-30 12:48:37 +0000

    Add custom off-mode charging screen, original commit : https://github.com/CyanogenMod/android_vendor_cm/commit/75956207503ca4f5023c1d2b587594c517e7e28f

--- common.mk
+++ common.mk
@@ -241,3 +241,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
+
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
