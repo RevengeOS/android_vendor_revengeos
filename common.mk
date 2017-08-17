0faf37f72edc58cf23d0f82cf2759498224230a9
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:44:27 +0200

    vendor: Move ro.device.cache_dir to common.mk
    Change-Id: Ic2e2284de064c6f9fa1aa8d9ba6b7af19c671c4e

--- common.mk
+++ common.mk
@@ -23,6 +23,15 @@ PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
+
+ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES  += \
+    ro.device.cache_dir=/data/cache
+else
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES  += \
+    ro.device.cache_dir=/cache
+endif
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
