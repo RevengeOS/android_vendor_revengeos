f61fa828989363cbf659865df7bfc457a9c3d3f7
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put citrus variables in the "Citrus" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Citrus structure
 citrus_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Citrus": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
