eabd6065b9c7986e79a45cca9b6b00397781499b
Carsten Hauge <carsten.hauge@sonymobile.com>
2017-10-17 15:49:16 -0400

    Import charging sounds
    Extracted from this commit
    https://android-review.googlesource.com/#/c/platform/frameworks/base/+/243420/
    
    Change-Id: Idba17bb1b6d2f6899bcd98d2fe7336d31835cb42

--- config/common.mk
+++ config/common.mk
@@ -52,6 +52,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/media/audio/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/aosp/prebuilt/common/media/audio/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
+
 # Additional packages
 -include vendor/revengeos/config/packages.mk
 
