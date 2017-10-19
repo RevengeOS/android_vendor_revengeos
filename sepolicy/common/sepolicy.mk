4d5f1eef67c1cecd467eca3166c27804de99f881
Ethan Chen <intervigil@gmail.com>
2017-10-18 19:11:22 -0700

    sepolicy: Move policies to platform
    Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- sepolicy/common/sepolicy.mk
+++ sepolicy/common/sepolicy.mk
@@ -3,5 +3,9 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    vendor/citrus/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/citrus/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/citrus/sepolicy/common/private
+
