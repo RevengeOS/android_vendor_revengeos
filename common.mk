Cardinal-AOSP/vendor_cardinal

vendor: copy premade changelog on official builds
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

common.mk
@@ -58,6 +58,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cardinal/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
+# Changelog
+ifeq ($(CARDINAL_RELEASE),true)
+PRODUCT_COPY_FILES +=  \
+    vendor/cardinal/prebuilt/common/etc/Changelog.txt:system/etc/Changelog.txt
+else
+GENERATE_CHANGELOG := true
+endif
+
 # Dialer fix
 PRODUCT_COPY_FILES +=  \
     vendor/cardinal/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml

RevengeOS/android_vendor_revengeos

common.mk
@@ -79,6 +79,14 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/citrus/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
+# Changelog
+ifeq ($(CITRUS_RELEASE),true)
+PRODUCT_COPY_FILES +=  \
+    vendor/citrus/prebuilt/common/etc/Changelog.txt:system/etc/Changelog.txt
+else
+GENERATE_CHANGELOG := true
+endif
+
 # Dialer fix
 PRODUCT_COPY_FILES +=  \
     vendor/citrus/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml