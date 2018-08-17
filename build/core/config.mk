2d9b535c1910e79ae3a5770b50c94bbb691c7b18
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    vendor/citrus: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,2 +1,7 @@
 # Rules for MTK targets
 include $(TOPDIR)vendor/citrus/build/core/qcom_target.mk
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
