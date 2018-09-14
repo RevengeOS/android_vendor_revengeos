2ae15a76f2ebc557863825ac339517dcd6231d64
Lucchetto <lucchetto.tie@live.com>
2018-09-14 19:59:28 +0200

    Our overlays

--- config/common_tablet.mk
+++ config/common_tablet.mk
@@ -1,7 +1,4 @@
 # Common settings and files
 -include vendor/revengeos/config/common.mk
 
-# Add tablet overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/revengeos/overlay/common_tablet
-
 PRODUCT_CHARACTERISTICS := tablet
