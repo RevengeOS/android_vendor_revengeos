e923cb172424f92904e342957ba79bedae86b0e8
Lucchetto <lucchetto.tie@live.com>
2018-09-14 20:30:34 +0200

    Nuke CR sounds

--- config/common.mk
+++ config/common.mk
@@ -52,11 +52,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Charging sounds
-PRODUCT_COPY_FILES += \
-    vendor/revengeos/sounds/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
-    vendor/revengeos/sounds/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
-
 # Additional packages
 -include vendor/revengeos/config/packages.mk
 
