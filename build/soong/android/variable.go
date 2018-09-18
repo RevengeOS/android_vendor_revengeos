39c0758ba6f5f95d64f3e0fb5840adb2c0dcb1d1
Lucchetto <lucchetto.tie@live.com>
2018-09-18 20:25:03 +0200

    Use AEX soong configuration

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,4 +1,4 @@
-package android
+package android_aosp
 type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
@@ -8,72 +8,40 @@ type Product_variables struct {
 		Cflags []string
 	}
 
-	Needs_legacy_camera_hal1_dyn_native_handle struct {
-		Cppflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
 
-	Mtk_hardware struct {
-		Cflags []string
-	}
-
-	Cant_reallocate_omx_buffers struct {
-		Cflags []string
-	}
-
-	Qcom_bsp_legacy struct {
+	Target_shim_libs struct {
 		Cppflags []string
-	}
-
-	Qti_flac_decoder struct {
-		Cflags []string
 	}
 
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
 
-	Target_uses_qsml struct {
-		Cflags []string
-		Shared_libs []string
-		Header_libs []string
-		Required []string
-	}
-
-	Target_uses_eigen struct {
-		Shared_libs []string
-		Required []string
-	}
-
-	Target_shim_libs struct {
+	Uses_nvidia_enhancements struct {
 		Cppflags []string
+	}
+
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
+
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
 	}
 }
 
 type ProductVariables struct {
-	Additional_gralloc_10_usage_bits  			*string `json:",omitempty"`
-	Has_legacy_camera_hal1						*bool `json:",omitempty"`
-	Needs_legacy_camera_hal1_dyn_native_handle	*bool `json:",omitempty"`
-	Uses_media_extensions						*bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
-	Specific_camera_parameter_library			*string `json:",omitempty"`
-	Needs_text_relocations						*bool `json:",omitempty"`
-	Mtk_hardware								*bool `json:",omitempty"`
-	QTIAudioPath								*string `json:",omitempty"`
-	QTIDisplayPath								*string `json:",omitempty"`
-	QTIMediaPath								*string `json:",omitempty"`
-	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
-	Qcom_bsp_legacy								*bool `json:",omitempty"`
-	Qti_flac_decoder							*bool `json:",omitempty"`
-	Target_uses_qsml							*bool `json:",omitempty"`
-	Target_uses_eigen							*bool `json:",omitempty"`
-	Target_use_sdclang							*bool `json:",omitempty"`
-	Target_shim_libs							*string `json:",omitempty"`
+	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Needs_text_relocations  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Target_shim_libs  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_nvidia_enhancements  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,29 +1,19 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-$(eval _contents := $$(_contents)"Carbon": {$$(newline))
+_contents := $(_contents)    "Aosp":{$(newline)
 
-$(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false)))
-$(call add_json_bool,	Needs_legacy_camera_hal1_dyn_native_handle,	$(filter true,$(if $(filter true,$(TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE)),true,false)))
-$(call add_json_bool,	Uses_media_extensions,						$(filter true,$(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false)))
-$(call add_json_bool,	Uses_generic_camera_parameter_library		$(filter true,$(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)))
-$(call add_json_bool,	Needs_text_relocations,						$(filter true,$(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)))
-$(call add_json_bool,	Mtk_hardware,								$(filter true,$(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false)))
-$(call add_json_bool,	BoardUsesQTIHardware,						$(filter true,$(if $(BOARD_USES_QTI_HARDWARE),true,false)))
-$(call add_json_bool,	Cant_reallocate_omx_buffers,				$(filter true,$(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false)))
-$(call add_json_bool,	Qcom_bsp_legacy,							$(filter true,$(if $(TARGET_USES_QCOM_BSP_LEGACY),true,false)))
-$(call add_json_bool,	Qti_flac_decoder,							$(filter true,$(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false)))
-$(call add_json_bool,	TargetUsesProprietaryLibs,					$(filter true,$(if $(strip $(TARGET_USES_PROPRIETARY_LIBS)),true,false)))
-$(call add_json_bool,	Target_uses_qsml,							$(filter true,$(if $(strip $(TARGET_USES_QSML)),true,false)))
-$(call add_json_bool,	Target_uses_eigen,							$(filter true,$(if $(strip $(TARGET_USES_QSML)),false,true)))
-$(call add_json_bool,	Target_use_sdclang,							$(filter true,$(if $(strip $(TARGET_USE_SDCLANG)),true,false)))
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
-$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits, 	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
 
-$(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
-$(call add_json_str,	Libart_img_base,							$(LIBART_IMG_BASE))
-$(call add_json_str,	QTIAudioPath,								$(call project-path-for,qcom-audio))
-$(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display))
-$(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
-$(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
-
-$(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))
+_contents := $(_contents)    },$(newline)
