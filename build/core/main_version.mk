8bf008bbafcea9e762144767dee184c0de07b58a
Adarsh-MR <adarshmr1998@gmail.com>
2018-09-18 09:17:43 +0000

    vendor/citrus: add CAF Revision prop
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,6 +1,7 @@
 # Citrus sprcific build properties
 ADDITIONAL_BUILD_PROPERTIES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.caf.revision=$(CAF_REVISION) \
     ro.citrus.buildtype=$(CITRUS_BUILD_TYPE) \
     ro.citrus.flavour=$(CITRUS_VERSION_FLAVOUR) \
     ro.citrus.version=$(CITRUS_VERSION_CODENAME)-$(CITRUS_VERSION_FLAVOUR) \
--- common.mk
+++ common.mk
@@ -239,6 +239,7 @@ endif
 include vendor/citrus/build/sdclang/sdclang.mk
 
 # Citrus-CAF versions.
+CAF_REVISION := LA.UM.7.3.r1-05100-sdm845.0
 CITRUS_VERSION_FLAVOUR = KeyLime
 CITRUS_VERSION_CODENAME := 5.0
 PLATFORM_VERSION_FLAVOUR := Pie
