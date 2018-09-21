3713cae0c44fb19f228a8956c86ef12cc693e79e
Adarsh-MR <adarshmr1998@gmail.com>
2018-09-21 14:37:04 +0000

    vendor/citrus: soong: Add target_uses_color_metadata soong flag
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -15,6 +15,9 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_uses_color_metadata struct {
+		Cppflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -37,6 +40,7 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_uses_color_metadata  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
