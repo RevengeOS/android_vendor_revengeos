22fec20396d5af7ba104f7b38519836c2094134a
Zhenxiang <lucchetto.tie@live.com>
2019-03-01 15:35:55 +0000

    Always ignore neverallows

--- build/core/config.mk
+++ build/core/config.mk
@@ -2,6 +2,4 @@
 include $(TOPDIR)vendor/revengeos/build/core/qcom_target.mk
 
 # We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
-endif
