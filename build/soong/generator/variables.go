144f46737d895672daec2d8936c006e337d28ad7
Lucchetto <lucchetto.tie@live.com>
2019-09-10 20:46:34 +0200

    vendor: revert to arrow soong
    Change-Id: I112b24b2a45dddcaef7d0fd8d300d0c93ff620cd
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func revengeosExpandVariables(ctx android.ModuleContext, in string) string {
-	revengeosVars := ctx.Config().VendorConfig("revengeosVarsPlugin")
+func arrowExpandVariables(ctx android.ModuleContext, in string) string {
+	arrowVars := ctx.Config().VendorConfig("arrowVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if revengeosVars.IsSet(name) {
-			return revengeosVars.String(name), nil
+		if arrowVars.IsSet(name) {
+			return arrowVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
