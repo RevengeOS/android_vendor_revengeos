mirdaVG/platform_vendor_aosip

Kang privapp-permissions-elgoog.xml from mTresk and OmniROM
Co-authored-by: Maxim Tresk <djtresk@gmail.com>

config/common.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.opa.eligible_device=true
 
+# Google permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 

RevengeOS/android_vendor_revengeos

Change-Id: I3d033bbbba15189f35a81f7fdb38af9a14f97ded
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

config/common.mk
@@ -43,6 +43,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/revengeos/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
 
+# Google permissions
+PRODUCT_COPY_FILES += \
+    vendor/revengeos/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 