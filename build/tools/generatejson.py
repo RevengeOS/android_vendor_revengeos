b858d93923bc33028ccc8d75edf1244102b475cd
Ethan Halsall <ethan.halsall@gmail.com>
2020-04-30 12:19:13 +0100

    vendor: add push_update()
    - This will allow maintainer to automatically upload builds to OSDN.
    - Also will generate and push json to our offical devices repo
    
    Change-Id: I65d504a1083904a86201d8247e95981ed5a77ba7
    Co-authored-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>
    Signed-off-by: nysascape <jago@nysascape.digital>

---
+++ build/tools/generatejson.py
@@ -0,0 +1,40 @@
+import json
+import time
+import sys
+
+device = sys.argv[1]
+zipvar = sys.argv[2]
+version = sys.argv[3]
+size = sys.argv[4]
+md5 = sys.argv[5]
+
+donate_url = str(input("Enter donate url, leave blank for default: "))
+news_url = str(input("Enter channel url, leave blank for default: "))
+
+if not donate_url:
+	donate_url = "https://paypal.me/lucchetto"
+
+if not news_url:
+	news_url = "https://t.me/RevengeOSNews"
+
+# Data to be written
+dictionary ={
+	"error": False,
+	"donate_url": donate_url,
+	"website_url": "https://revengeos.com/",
+	"news_url": news_url,
+	"datetime": time.time(),
+	"filename": zipvar,
+	"id": "",
+	"size": size,
+	"url": "https://gensho.ftp.acc.umu.se/mirror/osdn.net/storage/g/r/re/revengeos/" + device + "/" + zipvar,
+	"version": version,
+	"filehash": md5
+}
+
+# Serializing json
+json_object = json.dumps(dictionary, indent = 4)
+
+# Writing to device.json
+with open("device.json", "w") as outfile:
+    outfile.write(json_object)
