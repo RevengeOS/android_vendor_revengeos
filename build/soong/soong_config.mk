20bd40c3521ecec2e1007ac9d2626f1d0e7b8075
TheScarastic <warabhishek@gmail.com>
2020-05-25 10:09:14 +0000

    soong: Add flag for fod extension
    Change-Id: Ib45706cc50fa88b9b05af1c68eec9d1f08d70569

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -15,6 +15,7 @@ $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFF
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
