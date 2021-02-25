9028b3bfb29dbde215116b0cd3d53e0610f9c0ca
Lucchetto <lucchetto.tie@live.com>
2021-02-25 11:09:27 +0100

    revengeos: don't ignore neverallows
        * Sepolicy builds just fine without
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- build/core/config.mk
+++ build/core/config.mk
@@ -2,6 +2,3 @@ BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/revengeos/build/core/system_rro.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/revengeos/build/core/qcom_target.mk
-
-# We modify several neverallows, so let the build proceed
-SELINUX_IGNORE_NEVERALLOWS := true
