a78a0a0fb08fae8adcf4fa71d359020877432a29
Giovix92 <ggualtierone@gmail.com>
2021-05-11 01:49:44 +0200

    vendor: build: re-enable the ability to set variants
    Useful whenever a device needs a special HAL in order to work properly.
    
    Signed-off-by: Giovix92 <ggualtierone@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -13,15 +13,48 @@ $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VA
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
+ifdef TARGET_OVERRIDE_BT_VENDOR
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/$(TARGET_OVERRIDE_BT_VENDOR))
+else
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+endif
+
+ifdef TARGET_OVERRIDE_DATA_IPA_CFG_MGR
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/$(TARGET_OVERRIDE_DATA_IPA_CFG_MGR))
+else
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+endif
+
+ifdef TARGET_OVERRIDE_DATASERVICES
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/$(TARGET_OVERRIDE_DATASERVICES))
+else
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+endif
+
+ifdef TARGET_OVERRIDE_POWER
+$(call set-device-specific-path,POWER,power,hardware/qcom-caf/$(TARGET_OVERRIDE_POWER))
+else
 $(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
+endif
+
+ifdef TARGET_OVERRIDE_THERMAL
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/$(TARGET_OVERRIDE_THERMAL))
+else
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+endif
+
+ifdef TARGET_OVERRIDE_VR
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/$(TARGET_OVERRIDE_VR))
+else
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+endif
+
+ifdef TARGET_OVERRIDE_WLAN
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/$(TARGET_OVERRIDE_WLAN))
+else
 $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+endif
 
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
-
 endif
