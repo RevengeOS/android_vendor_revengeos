85972e70eabf73195c1165b1e1c63056ed0a4c98
Giovix92 <ggualtierone@gmail.com>
2021-06-02 17:43:20 +0200

    vendor: roomservice: only remove custom manifest if existent
    Otherwise CI goes oof.

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -57,7 +57,8 @@ hardware_manifest = ".repo/manifests/snippets/hardware.xml"
 
 github_auth = os.getenv('GITHUB_API_TOKEN', None)
 
-os.remove(custom_local_manifest, True)
+if os.path.exists(custom_local_manifest):
+    os.remove(custom_local_manifest)
 local_manifests = '.repo/local_manifests'
 if not os.path.exists(local_manifests):
     os.makedirs(local_manifests)
